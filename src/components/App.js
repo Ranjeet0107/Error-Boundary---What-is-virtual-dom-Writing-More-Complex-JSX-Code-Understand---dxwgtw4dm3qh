455b2ce3177a7d8589fd34c98448dc6aa15fa216
Ranjeet <ranjeet.ggi@gmail.com>
2023-03-26 12:32:52 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
+import React from "react";
+import "../styles/App.css";
 import Component from "./Component";
 import ErrorBoundary from "./ErrorBoundary";
 const App = () => {
   let setFunc;
-
-  let changeFunc = (func) => {
+  const changeFunc = (func) => {
     setFunc = func;
   };
 
   const errorGenerator = () => {
     setFunc(true);
   };
-
   return (
     <div id="main">
       <h2>React Error Boundaries</h2>
@@ -23,4 +23,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
